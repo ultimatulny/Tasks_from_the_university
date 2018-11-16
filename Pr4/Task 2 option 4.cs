fecceb03aff780da440c3113ab6c5072c68b615f
Alexander <31167599+ultimatulny@users.noreply.github.com>
2018-11-16 20:13:49 +0500

    Update Task 2 option 4.cs

--- Pr4/Task 2 option 4.cs
+++ Pr4/Task 2 option 4.cs
@@ -2,27 +2,48 @@ using System;
 
 namespace Mycode
 {
-	class Program
-	{	
+    class Program
+    {
 
-		public static void Main(string[] args)
-		{
-			double x,sum = 0, max =0,before=0;
-			do{
-				x = System.Convert.ToDouble(Console.ReadLine());
-				if((x>max)&&(x!=before)){
-					sum = 0;
-					max = x;
-					sum++;
-				} else if((x == max) && (x!=0)){
-					sum++;	
-				}
-				before = x;
-			} while(x!=0);
-			Console.WriteLine("Наибольшее значение принимают: {0} element",sum);
-			Console.ReadKey();
-		}
-		
-		
-	}
+        public static void Main(string[] args)
+        {
+            double x, sum = 0, max = 0, before = 0;
+
+            x = System.Convert.ToDouble(Console.ReadLine());
+            if (x == 0)
+            {
+                Console.WriteLine("Total: {0} element", sum);
+                Environment.Exit(0);
+            }
+            else
+            {
+                max = x;
+                sum++;
+               
+            }
+
+            do
+            {
+                x = System.Convert.ToDouble(Console.ReadLine());
+                if (x == 0)
+                {
+                    Console.WriteLine("Total: {0} element", sum);
+                    Environment.Exit(0);
+                }
+                if (x > max)
+                {
+                    sum = 0;
+                    max = x;
+                    sum++;
+                } else if(x == max){
+                    sum++;
+                   
+                }
+                
+            } while (x != 0);
+            Console.WriteLine("Total: {0} element", sum);
+            Console.ReadKey();
+        }
+
+    }
 }
