ec2133f061209f417e00e22d81d8910afd713edf
Alexander <31167599+ultimatulny@users.noreply.github.com>
2018-11-17 18:06:12 +0500

    Pr5 option 7

---
+++ Pr5/Task 1 option 7.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Mycode
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            int n = System.Convert.ToInt32(Console.ReadLine()); // Задаем размер массива
+            int[] x = new int[n]; // Создаем массив
+            int check = 0;
+
+            for (int i = 0; i < n; i++) // Заполняем массив
+            {
+                Console.Write("Введите {0} элемент ", i);
+                x[i] = System.Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n-1; i++) // Проверяем порядок, если все элементы упорядочены по убыванию, то check == n-1
+            {
+                if (x[i] > x[i+1])
+                {
+                    check++;
+                }
+            }
+
+            if (check == n - 1) 
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+        }
+
+    }
+}
---
+++ Pr5/Task 2 option 7.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Mycode
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            int n = System.Convert.ToInt32(Console.ReadLine()); // Задаем размер массива
+            int[] x = new int[n]; // Создаем массив
+            int k = 0; // Кол-во ненулевых элементов, стоящих после положительных
+            double sum = 0; // Сумма ненулевых элементов, стоящих после положительных
+            double result = 0;
+
+
+            for (int i = 0; i < n; i++) // Заполняем массив
+            {
+                Console.Write("Введите {0} элемент ", i);
+                x[i] = System.Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n-1; i++) // Проверяем элементы
+            {
+               if((x[i] > 0) && (x[i+1] != 0))
+                {
+                    sum += x[i + 1];
+                    k++;
+                }
+            }
+
+            result = sum / k;
+            Console.WriteLine(result);
+        }
+
+    }
+}
