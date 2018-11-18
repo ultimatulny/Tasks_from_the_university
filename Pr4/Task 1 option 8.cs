659ec874fe402301a6ea9f79eb7d37425d44091d
Alexander <31167599+ultimatulny@users.noreply.github.com>
2018-11-18 21:11:24 +0500

    Pr4 option 8

---
+++ Pr4/Task 1 option 8.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Program
+{
+    class Program
+    {
+
+
+        public static double fact(double f)
+        {
+            double fa = 1;
+            for (int i = 1; i <= f; i++)
+            {
+                fa *= i;
+            }
+            return fa;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter E = ");
+            double e = System.Convert.ToDouble(Console.ReadLine());
+            double j = 1;
+            double n = (fact(j)) / fact((Math.Pow(2, j)));
+            double sum = 0;
+
+            while (n >= e)
+            {
+                Console.WriteLine("{0:0.000000000}", n);
+                sum += n;
+                j++;
+                n = (fact(j)) / fact((Math.Pow(2, j)));
+            }
+
+            Console.WriteLine("SUM= {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
