95942625bde2fa749fe915d901f04098ff36da9d
Alexander <31167599+ultimatulny@users.noreply.github.com>
2019-02-25 21:14:14 +0500

    Create FileStream.cs

---
+++ FileStream.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace JustFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8; // Кодировка для вывода в консоли
+
+            string path = @"E:\test\file.txt"; // Создание переменной, которая будет хранить путь к файлу
+
+            using (var sw = new StreamWriter(path, true, Encoding.UTF8)) // Запись в файл строки "Hello" n раз, т.к. есть true
+            {
+                sw.Write("Hello!");
+            }
+
+            using (var sr = new StreamReader(path, Encoding.UTF8)) // Чтение файла
+            {
+                var text = sr.ReadToEnd();
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
